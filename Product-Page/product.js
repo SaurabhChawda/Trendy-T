1f1c9b9abc0289433594fce1cf43dbdc153ec6b3
Saurabh chawda <saurabhchawda2000@gmail.com>
2022-02-09 13:48:48 +0530

    Style : Add Filtes in product page

--- Product-Page/product.js
+++ Product-Page/product.js
@@ -1,3 +1,5 @@
+// Hamburger
+
 const hamburgerMenu = document.querySelector('.hamburger')
 const hamburgerOpen = document.querySelector('.nav-bar__img--hamburger')
 const hamburgerClose = document.querySelector('.hamburger__close-btn')
@@ -23,3 +25,16 @@ hamburgerClose.addEventListener('click',()=>{
     hamburgerMenu.style.width="0vh"
 
 })
+
+
+// Sidebar Clear javaScript //
+
+const sidebarClearBtn = document.querySelector('.sidebar__btn--clear-filter')
+let sidebarInput = document.querySelectorAll('.sidebar__input')
+
+sidebarClearBtn.addEventListener('click',()=>{
+   sidebarInput.forEach(element => {
+      element.checked = false
+   });
+})
+
