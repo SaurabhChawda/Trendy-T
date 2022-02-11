0f8304d8910d62646a2e2361fd447134826a9025
Saurabh chawda <saurabhchawda2000@gmail.com>
2022-02-11 17:37:48 +0530

    Feat : Feat Signup page

---
+++ Signup-page/signup.js
@@ -0,0 +1,27 @@
+// Hamburger
+
+const hamburgerMenu = document.querySelector('.hamburger')
+const hamburgerOpen = document.querySelector('.nav-bar__img--hamburger')
+const hamburgerClose = document.querySelector('.hamburger__close-btn')
+
+hamburgerOpen.addEventListener('click',()=>{
+   hamburgerMenu.style.visibility = "visible";
+   hamburgerMenu.style.width="250px"
+
+   function myFunction(x) {
+      if (x.matches) {
+         hamburgerMenu.style.visibility = "hidden";
+      } else {
+         hamburgerMenu.style.visibility = "visible";
+      }
+    }
+    var x = window.matchMedia("(min-width:500px)")
+    myFunction(x)
+    x.addListener(myFunction)
+
+})
+hamburgerClose.addEventListener('click',()=>{
+    hamburgerMenu.style.visibility = "hidden"
+    hamburgerMenu.style.width="0vh"
+
+})
