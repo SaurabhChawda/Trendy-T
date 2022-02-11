a8799f0a55583469dcc31dcb610b2889133ef7bf
Saurabh chawda <saurabhchawda2000@gmail.com>
2022-02-11 22:24:19 +0530

    fix : fix preloader bug

--- wishlist-page/wishlist.js
+++ wishlist-page/wishlist.js
@@ -3,6 +3,7 @@
 const hamburgerMenu = document.querySelector('.hamburger')
 const hamburgerOpen = document.querySelector('.nav-bar__img--hamburger')
 const hamburgerClose = document.querySelector('.hamburger__close-btn')
+const preLoader = document.querySelector('.preloader')
 
 hamburgerOpen.addEventListener('click',()=>{
    hamburgerMenu.style.visibility = "visible";
